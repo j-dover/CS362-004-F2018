23502f3266d873352fbf5ea403fdc261c5000bc8
j-dover <j-dover@users.noreply.github.com>
2018-10-15 01:10:03 -0400

    refactored council_room to councilRoomEffect()

--- projects/aldoverj/dominion/dominion.c
+++ projects/aldoverj/dominion/dominion.c
@@ -708,14 +708,37 @@ int councilRoomEffect(int player, struct gameState *state, int handPos)
   return 0;  
 }
 
-int remodelEffect()
+int remodelEffect(int player, struct gameState *state, int handPos, int choice1, int choice2)
 {
-  
+  int j = state->hand[player][choice1];  //store card we will trash
+
+  if ( (getCost(state->hand[player][choice1]) + 2) > getCost(choice2) )
+    {
+      return -1;
+    }
+
+  gainCard(choice2, state, 0, player);
+
+  //discard card from hand
+  discardCard(handPos, player, state, 0);
+
+  //discard trashed card
+  for (int i = 0; i < state->handCount[player]; i++)
+    {
+      if (state->hand[player][i] == j)
+        {
+          discardCard(i, player, state, 0);
+          break;
+        }
+    }
+
+  return 0;
 }
 
 int villageEffect()
 {
-  
+
+  return 0;
 }
 
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
@@ -838,30 +861,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 			
     case remodel:
-      j = state->hand[currentPlayer][choice1];  //store card we will trash
-
-      if ( (getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(choice2) )
-	{
-	  return -1;
-	}
-
-      gainCard(choice2, state, 0, currentPlayer);
-
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-
-      //discard trashed card
-      for (i = 0; i < state->handCount[currentPlayer]; i++)
-	{
-	  if (state->hand[currentPlayer][i] == j)
-	    {
-	      discardCard(i, currentPlayer, state, 0);			
-	      break;
-	    }
-	}
-
-
-      return 0;
+      return remodelEffect(currentPlayer, state, handPos, choice1, choice2);
 		
     case smithy:
       return smithyEffect(currentPlayer, state, handPos);
