22f68d7478aab48ee9c8912430dddd95d590b87f
j-dover <j-dover@users.noreply.github.com>
2018-10-14 23:57:38 -0400

    removed unnecessary code

--- projects/aldoverj/dominion/dominion.c
+++ projects/aldoverj/dominion/dominion.c
@@ -694,12 +694,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     {
     case adventurer:
 	return adventurerEffect(currentPlayer, state, drawntreasure, temphand, z);
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
 			
     case council_room:
       //+4 Cards
