e10874f34a317df5d71927abbbc1c5457353afe4
j-dover <j-dover@users.noreply.github.com>
2018-10-15 01:22:54 -0400

    refactored village to villageEffect()

--- projects/aldoverj/dominion/dominion.c
+++ projects/aldoverj/dominion/dominion.c
@@ -735,9 +735,16 @@ int remodelEffect(int player, struct gameState *state, int handPos, int choice1,
   return 0;
 }
 
-int villageEffect()
+int villageEffect(int player, struct gameState *state, int handPos)
 {
+  //+1 Card
+  drawCard(player, state);
 
+  //+2 Actions
+  state->numActions = state->numActions + 2;
+
+  //discard played card from hand
+  discardCard(handPos, player, state, 0);
   return 0;
 }
 
@@ -867,15 +874,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return smithyEffect(currentPlayer, state, handPos);
       	
     case village:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+2 Actions
-      state->numActions = state->numActions + 2;
-			
-      //discard played card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+      return villageEffect(currentPlayer, state, handPos);
 		
     case baron:
       state->numBuys++;//Increase buys by 1!
