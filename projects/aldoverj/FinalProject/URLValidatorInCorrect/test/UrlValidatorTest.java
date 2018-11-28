328b34fd58a4109a279fb00dca50e09d3c01c541
j-dover <j-dover@users.noreply.github.com>
2018-11-27 22:29:36 -0500

    add manual test

--- projects/aldoverj/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/aldoverj/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -17,12 +17,53 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-   
+   public void assertUrlTest(boolean expectedResult, boolean validatorResult, String urlInput) {
+	   if (java.util.Objects.equals(expectedResult, validatorResult)) {
+		   System.out.println("PASS Manual Test, URL: " + urlInput);
+	   }
+	   else {
+		   System.out.println("FAIL Manual Test, Expected Result: " + expectedResult + ", URL: " + urlInput);
+	   }
+   }
    
    public void testManualTest()
    {
 //You can use this function to implement your manual testing	   
+	   UrlValidator urlVal = new UrlValidator();
+	   String urlInput;
+	   boolean expectedResult;
+	   boolean validatorResult;
 	   
+	   // Testing Valid URLs
+	   System.out.println("-- Manual Testing Valid URLs --");
+	   expectedResult = true;
+	   urlInput = "https://valid-test1.com";
+	   validatorResult = urlVal.isValid(urlInput);
+	   assertUrlTest(expectedResult, validatorResult, urlInput);
+	   
+	   urlInput = "hftp://valid-test2.com:80/test?action=view";
+	   validatorResult = urlVal.isValid(urlInput);
+	   assertUrlTest(expectedResult, validatorResult, urlInput);
+	   
+	   urlInput = "http://valid-test3.com/";
+	   validatorResult = urlVal.isValid(urlInput);
+	   assertUrlTest(expectedResult, validatorResult, urlInput);
+	   
+	   // Testing Invalid URLs
+	   System.out.println("-- Manual testing Invalid URLs --");
+	   expectedResult = false;
+	   urlInput = "//valid-test1.com";		// Missing scheme
+	   validatorResult = urlVal.isValid(urlInput);
+	   assertUrlTest(expectedResult, validatorResult, urlInput);
+	   
+	   urlInput = "tp://valid-test2.com:80/test?action=view";	// Invalid scheme
+	   validatorResult = urlVal.isValid(urlInput);
+	   assertUrlTest(expectedResult, validatorResult, urlInput);
+	   
+	   urlInput = "http://valid-test3/";	// Missing component in authority
+	   validatorResult = urlVal.isValid(urlInput);
+	   assertUrlTest(expectedResult, validatorResult, urlInput);
+	      
    }
    
    
